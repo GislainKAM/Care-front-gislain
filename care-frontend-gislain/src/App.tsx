b6c5119692c92232579d3dd1892752432f635258
GislainKAM <gislainkamfotso@gmail.com>
2025-10-28 17:07:14 +0100

    mise en place compléte de l'environement de développement

--- care-frontend-gislain/src/App.tsx
+++ care-frontend-gislain/src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1> HELLO WORLD </h1>
+      <h1> HELLO WORLD </h1>
+      <h2>Call me Ing Manip</h2>
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
